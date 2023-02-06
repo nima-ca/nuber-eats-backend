e983b1e69b2aaef029eee5c2ef2e327169cbc2d9
nima nekouei <nekoueinima@protonmail.com>
2023-02-06 23:19:22 +0330

    [Feat]: add unit tests for login and findbyId methods

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -30,6 +30,7 @@ describe('UsersService', () => {
   let usersRepo: MockReposetory<User>;
   let verificationRepo: MockReposetory<Verification>;
   let mailService: MailService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -58,6 +59,7 @@ describe('UsersService', () => {
     usersRepo = module.get(getRepositoryToken(User));
     verificationRepo = module.get(getRepositoryToken(Verification));
     mailService = module.get<MailService>(MailService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
@@ -146,6 +148,72 @@ describe('UsersService', () => {
       usersRepo.findOne.mockRejectedValue(new Error('A dummy error'));
       const result = await uesrService.createAccount(createAccountArgs);
       expect(result.ok).toBeFalsy();
+    });
+  });
+
+  describe('login', () => {
+    const credentials = { email: 'test@example.com', password: '12345678' };
+    const userId = 1;
+
+    const mockedUser = {
+      id: userId,
+      checkPassword: jest.fn(() => Promise.resolve(false)),
+    };
+
+    it("should fail if user doesn't exist", async () => {
+      usersRepo.findOne.mockResolvedValue(undefined);
+      const result = await uesrService.login(credentials);
+
+      expect(usersRepo.findOne).toBeCalledTimes(1);
+      expect(usersRepo.findOne).toBeCalledWith({
+        where: { email: credentials.email },
+        select: { password: true, id: true },
+      });
+      expect(result.ok).toBeFalsy();
+    });
+
+    it('should fail if the password is Wrong', async () => {
+      usersRepo.findOne.mockResolvedValue(mockedUser);
+
+      const result = await uesrService.login(credentials);
+      expect(result.ok).toBeFalsy();
+    });
+
+    it('should return JWT', async () => {
+      mockedUser.checkPassword = jest.fn(() => Promise.resolve(true));
+      usersRepo.findOne.mockResolvedValue(mockedUser);
+
+      const result = await uesrService.login(credentials);
+      expect(jwtService.sign).toBeCalledTimes(1);
+      expect(jwtService.sign).toBeCalledWith({ id: userId });
+      expect(result.ok).toBe(true);
+    });
+
+    it('should fail if there was an error', async () => {
+      usersRepo.findOne.mockRejectedValue(new Error());
+      const result = await uesrService.login(credentials);
+      expect(result.ok).toBeFalsy();
+    });
+  });
+
+  describe('findById', () => {
+    const id = 5;
+
+    it('should return undefined if user is not found', async () => {
+      usersRepo.findOne.mockResolvedValue(undefined);
+      const result = await uesrService.findById(id);
+      expect(result).toBeFalsy();
+    });
+
+    it('should return a user if found', async () => {
+      const mockedUser = { id, email: 'test@example.com' };
+      usersRepo.findOne.mockResolvedValue(mockedUser);
+
+      const result = await uesrService.findById(id);
+
+      expect(usersRepo.findOne).toHaveBeenCalled();
+      expect(usersRepo.findOne).toHaveBeenCalledWith({ where: { id } });
+      expect(result).toEqual(mockedUser);
     });
   });
 });
